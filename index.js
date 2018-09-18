90f5a3b62228aebdde249f5d69912469b10b2626
Ezra Schwepker <ezraschwepker@gmail.com>
2018-09-18 14:55:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,12 +18,15 @@ function nowServing(line) {
 function currentLine(line) {
   if (line.length == 0) return "The line is currently empty.";
   
+  let array = [];
   let lineList = "The line is currently:"
-  for (let i = 0; i < line.length; i++) {
-    lineList = `${lineList} ${i + 1}. ${line[i]}`
-    if (i < line.length - 1) {
-      lineList = lineList + ",";
-    } 
-  }
-  return lineList;
+  // for (let i = 0; i < line.length; i++) {
+  //   // lineList = `${lineList} ${i + 1}. ${line[i]}`
+  //   array.push(`${i + 1}. ${line[i]}`);
+  //   /* if (i < line.length - 1) {
+  //     lineList = lineList + ",";
+  //   } */
+    
+  // }
+  return `${lineList} ${array.join(", ")}`;
 }
